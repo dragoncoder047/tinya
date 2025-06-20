a133ff5cf5b9a30acf6d035207fe10fe28efcb91
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2025-06-20 17:09:01 -0400

    fix unisons
    thanks @slarmoo for the math help

--- src/effectMacros.ts
+++ src/effectMacros.ts
@@ -1,4 +1,3 @@
-import { cos, PI } from "./math";
 import type { NodeParameter, NodeTree } from "./types";
 import { isNumber } from "./utils";
 
@@ -6,9 +5,9 @@ export const unisons = (oscTemplate: NodeTree, voices = 1, spread = 0, offset = 
     const out: Partial<NodeTree> = [];
     const baseFreq = oscTemplate[freqIndex];
     for (var i = 1; i <= voices; i++) {
-        const selfOffset = (offset ?? 0) + (i > 1 ? -cos(PI * i) * ((i / 2) | 0) * spread : 0);
-        const freqOffset = Math.pow(2, selfOffset / 12);
-        out[i] = oscTemplate.with(freqIndex, isNumber(baseFreq) ? baseFreq * freqOffset : [[], baseFreq, freqOffset]);
+        const offsetSemitones = offset + spread * (i - 1) / voices;
+        const offsetFactor = Math.pow(2, offsetSemitones / 12);
+        out[i] = oscTemplate.with(freqIndex, isNumber(baseFreq) ? baseFreq * offsetFactor : [[], baseFreq, offsetFactor]);
         if (i > 1 && sign !== 1) out[i] = [[], sign, out[i]];
     }
     return amp !== 1 ? [[], amp, out as NodeTree] : out as NodeTree;
